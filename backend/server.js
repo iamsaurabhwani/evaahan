0c0ecde4c5cc9f03dd19f962124b1f38aca46a57
iamsaurabhwani <saurabhwani52000@gmail.com>
2021-02-02 09:52:58 +0530

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/Evaahan', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/evaahan', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
